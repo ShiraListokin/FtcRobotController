d63dbe4c9e5d9a2de1140b484d4d7b065f66f7e8
ShiraListokin <26slistokin@gannacademy.org>
2024-04-07 22:31:00 -0400

    Update Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -5,16 +5,12 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class Test extends OpMode{
 
     DcMotor RF;
-    DcMotor RB;
-    DcMotor LF;
 
 
     @Override
     public void init() {
 
         RF = hardwareMap.get(DcMotor.class, "RF");
-        RB = hardwareMap.get(DcMotor.class, "RB");
-        LF = hardwareMap.get(DcMotor.class, "LF");
 
     }
 
@@ -22,8 +18,6 @@ public class Test extends OpMode{
     public void loop() {
 
         RF.setPower(1);
-        RB.setPower(1);
-        LF.setPower(1);
 
     }
 }
